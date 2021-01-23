387b9b579b358ccde7b733614ced1be468d7518f
DanielShinoda <danielkhaybulin@gmail.com>
2021-01-23 21:32:03 +0500

    fix

--- Build/Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/PathPlanning2D
-# Build directory: C:/PathPlanning2D/Build/Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Src")
-subdirs("Tests")
--- Build/Release/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/PathPlanning2D/Src
-# Build directory: C:/PathPlanning2D/Build/Release/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
